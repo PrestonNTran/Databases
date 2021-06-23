4aabfe03ec0f4a006b176f96555f8f0fbf7d44e7
Anubhav Parida <24parida@elmbrookstudents.org>
2021-06-23 09:15:16 -0500

    s

---
+++ CodeTheWay/CodeTheWay.Web.Ui/Models/ShippingContainer.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeTheWay.Web.Ui.Models
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
