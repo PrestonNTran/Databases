866e9abb4a0e14ebb6b2af1c335115ed2ad6a40e
Emily <eaberger2@wisc.edu>
2021-06-23 10:10:49 -0500

    View Model

--- CodeTheWay/CodeTheWay.Web.Ui/Controllers/StudentsController.cs
+++ CodeTheWay/CodeTheWay.Web.Ui/Controllers/StudentsController.cs
@@ -23,29 +23,59 @@ namespace CodeTheWay.Web.Ui.Controllers
         }
         public async Task<IActionResult> Create()
         {
-            return View(new Student());
+            return View(new StudentRegistrationViewModel());
         }
         [HttpPost]
-        public async Task<IActionResult> Register(Student model)
+        public async Task<IActionResult> Register(StudentRegistrationViewModel model)
         {
             if (ModelState.IsValid)
             {
-                var student = await StudentService.Create(model);
-                return RedirectToAction("Index");
+                if (model.Age < 19)
+                {
+                    Student student = new Student()
+                    {
+                        Id = model.Id,
+                        LastName = model.LastName,
+                        FirstMidName = model.FirstName
+                    };
+                    var newStudent = await StudentService.Create(student);
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return RedirectToAction("Index");
+                }
+                
             }
             return View(model);
         }
         public async Task<IActionResult> Edit(Guid id)
         {
             var student = await StudentService.GetStudent(id);
-            return View(student);
+            StudentRegistrationViewModel stu = new StudentRegistrationViewModel()
+            {
+                Id = student.Id,
+                FirstName = student.FirstMidName,
+                LastName = student.LastName,
+                Age = 0,
+            };
+            return View(stu);
         }
         [HttpPost]
-        public async Task<IActionResult> UpDate(Student model)
+        public async Task<IActionResult> UpDate(StudentRegistrationViewModel model)
         {
             if (ModelState.IsValid)
             {
-                var student = await StudentService.Update(model);
+                if(model.Age < 19)
+                {
+                    Student stu = new Student()
+                    {
+                        Id = model.Id,
+                        LastName = model.LastName,
+                        FirstMidName = model.FirstName,
+                    };
+                    var student = await StudentService.Update(stu);
+                }
                 return RedirectToAction("Index");
             }
             return View(model);
@@ -53,7 +83,15 @@ namespace CodeTheWay.Web.Ui.Controllers
         public async Task<IActionResult> Details(Guid id)
         {
             var student = await StudentService.GetStudent(id);
-            return View(student);
+            StudentRegistrationViewModel newStu = new StudentRegistrationViewModel()
+            {
+                Id = student.Id,
+                FirstName = student.FirstMidName,
+                LastName = student.LastName,
+                Age = 0,
+            };
+
+            return View(newStu);
         }
         public async Task<IActionResult> Delete(Guid id)
         {
